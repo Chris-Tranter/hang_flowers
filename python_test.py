22a2bfb2cf9fdacd925ce2e4cde5ab5006e7fd3b
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-06-01 10:22:30 +0100

    started importing a list of flowers for the game

---
+++ python_test.py
@@ -0,0 +1,6 @@
+                #allows for random choices to be called from inside a list
+import random
+                #the list which will be randomised
+flower_variants = [ 
+    'rose', 'dandelion', 'buttercup', 'daisy', 'foxglove'
+    ]
